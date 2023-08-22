bec35dee2d4c03a923d157232627a615329a76f1
Enoisong <enobongitiaba988@gmail.com>
2023-08-22 10:58:11 -0700

    setup

---
+++ config/routes.rb
@@ -0,0 +1,8 @@
+Rails.application.routes.draw do
+  get 'pages/blog'
+  # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
+
+  # Defines the root path route ("/")
+  # root "articles#index"
+  root 'pages#blog'
+end
