d8001a0eba59fa4a386eeeee58b59bd89be34169
Enoisong <enobongitiaba988@gmail.com>
2023-08-23 20:54:42 -0700

    add custom methods

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,8 +1,7 @@
 class User < ApplicationRecord
     has_many :posts, foreign_key: 'author_id', dependent: :destroy
     has_many :comments, foreign_key: 'author_id', dependent: :destroy
-    has_many :likes, foreign_key: 'author_id', dependent: :destroy
-end
+    has_many :likes, foreign_key: 'author_id', dependent: :destroy 
 
 def recent_posts
     posts.last(3).reverse
