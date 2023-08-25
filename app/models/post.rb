c8efe5f5e5d850930ebf392b60ada8b70601ecb8
Enoisong <enobongitiaba988@gmail.com>
2023-08-24 19:22:19 -0700

    fix linters

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,4 +1,4 @@
- class Post < ApplicationRecord
+class Post < ApplicationRecord
   belongs_to :author, class_name: 'User'
   has_many :comments, dependent: :destroy
   has_many :likes, dependent: :destroy
