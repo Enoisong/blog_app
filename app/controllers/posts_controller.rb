7f8ef72b3d7445379ac34057e37bb7ea6c3f5d8d
Enoisong <enobongitiaba988@gmail.com>
2023-08-28 20:36:08 -0700

    Update README file

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,11 +1,11 @@
 class PostsController < ApplicationController
-    def index
-        @user = User.find_by_id(params[:user_id])
-        posts = @user.posts
-    end
+  def index
+    @user = User.find_by_id(params[:user_id])
+    @user.posts
+  end
 
-    def show
-        @user = User.find_by_id(params[:user_id])
-        @post = @user.posts.find_by_id(params[:post_id])
-    end
+  def show
+    @user = User.find_by_id(params[:user_id])
+    @post = @user.posts.find_by_id(params[:post_id])
+  end
 end
