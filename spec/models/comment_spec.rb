97bc5b7ccd48934050da6251ae947312f7347fdc
Enoisong <enobongitiaba988@gmail.com>
2023-08-29 18:14:39 -0700

    update multiple get methods

--- spec/models/comment_spec.rb
+++ spec/models/comment_spec.rb
@@ -1,26 +1,7 @@
-# require 'rails_helper'
-
-# RSpec.describe Comment, type: :model do
-#   it 'should update the comments_counter of the post' do
-#     user = User.create(name: 'enoisong', photo: 'person.jpg', bio: 'Software developer', posts_counter: 0)
-#     post = Post.create(author: user, title: 'Hello', text: 'This is my first post', comments_counter: 0,
-#                        likes_counter: 0)
-#     comment = described_class.new(post:, author: user, text: 'This is my first comment')
-
-#     expect(post.comments_counter).to eq 0
-#     comment.save
-#     post.reload
-#     expect(post.comments_counter).to eq 1
-
-#     comment.destroy
-#     post.reload
-#     expect(post.comments_counter).to eq 0
-#   end
-# end
 require 'rails_helper'
 
 RSpec.describe Comment, type: :model do
-  user = User.create(name: 'anbehindY', photo: 'person.jpg', bio: 'Full-stack developer', posts_counter: 0)
+  user = User.create(name: 'enoisong', photo: 'person.jpg', bio: 'Software developer', posts_counter: 0)
   post = Post.create(author: user, title: 'Hello', text: 'This is my first post', comments_counter: 0, likes_counter: 0)
   subject { described_class.new(post: post, author: user, text: 'This is my first comment') }
 
