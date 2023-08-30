11e379ea3d2a8d3295e4bdcb8862d282b6f78049
Enoisong <enobongitiaba988@gmail.com>
2023-08-29 17:55:09 -0700

    update multiple get methods

--- spec/requests/users_request_spec.rb
+++ spec/requests/users_request_spec.rb
@@ -1,10 +1,3 @@
-# require 'rails_helper'
-
-# RSpec.describe 'Users', type: :request do
-#   describe 'GET /index' do
-#     pending "add some examples (or delete) #{__FILE__}"
-#   end
-# end
 require 'rails_helper'
 
 RSpec.describe 'User', type: :request do
