24706cd793715349aea4c84e208a1b8da346cf70
Enoisong <enobongitiaba988@gmail.com>
2023-08-29 18:54:32 -0700

    fix linters

--- spec/models/comment_spec.rb
+++ spec/models/comment_spec.rb
@@ -3,7 +3,7 @@ require 'rails_helper'
 RSpec.describe Comment, type: :model do
   user = User.create(name: 'enoisong', photo: 'person.jpg', bio: 'Software developer', posts_counter: 0)
   post = Post.create(author: user, title: 'Hello', text: 'This is my first post', comments_counter: 0, likes_counter: 0)
-  subject { described_class.new(post: post, author: user, text: 'This is my first comment') }
+  subject { described_class.new(post:, author: user, text: 'This is my first comment') }
 
   context '#comments_counter' do
     it 'should update the comments_counter of the post' do
