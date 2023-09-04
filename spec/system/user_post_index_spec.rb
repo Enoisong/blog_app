4ee438f5a76fb75153d4d03a67466a2f22be13f8
Enoisong <enobongitiaba988@gmail.com>
2023-09-04 03:43:37 -0700

    Add Integration tests for Post Index Page

---
+++ spec/system/user_post_index_spec.rb
@@ -0,0 +1,91 @@
+require 'rails_helper'
+
+RSpec.describe 'User Post Index Page', type: :system do
+  before do
+    driven_by(:rack_test)
+    @user1 = User.create!(name: 'jane', photo: 'person.jpg', bio: 'Software developer')
+    @user2 = User.create!(name: 'Jessica', photo: 'person2.jpg', bio: 'Software developer')
+    @post1 = Post.create!(author: @user1, title: 'First Post', text: 'This is my first post!')
+    @post2 = Post.create!(author: @user2, title: 'first post', text: 'This is my first post!')
+    @post3 = Post.create!(author: @user1, title: 'Second Post', text: 'This is my second post!')
+  end
+
+  context 'when the page is loaded' do
+    it 'shows the username of the user' do
+      visit user_posts_path(@user1)
+
+      expect(page).to have_content(@user1.name)
+    end
+
+    it 'shows the profile picture of the user' do
+      visit user_posts_path(@user1)
+
+      expect(page).to have_css("img[src*='person.jpg']")
+    end
+
+    it 'shows the number of posts the user has' do
+      visit user_posts_path(@user1)
+
+      expect(page).to have_content(@user1.posts.count)
+    end
+
+    it 'shows the title of the post' do
+      visit user_posts_path(@user1)
+
+      expect(page).to have_content(@user1.posts[0].title)
+      expect(page).to have_content(@user1.posts[1].title)
+    end
+
+    it 'shows the text of the post' do
+      visit user_posts_path(@user1)
+
+      expect(page).to have_content(@user1.posts[0].text)
+      expect(page).to have_content(@user1.posts[1].text)
+    end
+
+    it 'shows the first comments of each post' do
+      Comment.create!(author: @user1, post: @post1, text: 'this is my first comment')
+      Comment.create!(author: @user1, post: @post1, text: 'this is my second comment')
+      visit user_posts_path(@user1)
+
+      expect(page).to have_content(@user1.posts[0].comments[0])
+      expect(page).to have_content(@user1.posts[0].comments[1])
+    end
+
+    it 'shows the comment count of each post' do
+      Comment.create!(author: @user1, post: @post1, text: 'this is my first comment')
+      Comment.create!(author: @user1, post: @post1, text: 'this is my second comment')
+      visit user_posts_path(@user1)
+
+      expect(page).to have_content(@user1.posts[0].comments_counter)
+    end
+
+    it 'shows the like count of each post' do
+      Like.create!(author: @user1, post: @post1)
+      Like.create!(author: @user1, post: @post1)
+      Like.create!(author: @user1, post: @post2)
+      visit user_posts_path(@user1)
+
+      expect(page).to have_content(@user1.posts[0].likes_counter)
+      expect(page).to have_content(@user1.posts[1].likes_counter)
+    end
+  end
+
+  context 'when you click on a post' do
+    it 'redirects to the post1 show page' do
+      visit user_posts_path(@user1)
+
+      click_link(@user1.posts[0].title)
+
+      expect(page).to have_current_path(user_post_path(@user1, @post1))
+    end
+
+    it 'redirects to the post2 show page' do
+      visit user_posts_path(@user1)
+
+      click_link(@user1.posts[1].title)
+
+      expect(page).to have_current_path(user_post_path(@user1, @post3))
+    end
+  end
+end
---
+++ spec/system/user_show_spec.rb
@@ -0,0 +1,69 @@
+require 'rails_helper'
+
+RSpec.describe 'User Show Page', type: :system do
+  before do
+    driven_by(:rack_test)
+    @user = User.create!(name: 'Enoisong', photo: 'person.jpg', bio: 'Software developer')
+    @post = Post.create!(author: @user, title: 'First Post', text: 'This is my first post!')
+    @post2 = Post.create!(author: @user, title: 'Second Post', text: 'This is my second post!')
+    @post3 = Post.create!(author: @user, title: 'Third Post', text: 'This is my third post!')
+  end
+
+  context 'when the page is loaded' do
+    it 'shows the username' do
+      visit user_path(@user)
+
+      expect(page).to have_content(@user.name)
+    end
+
+    it 'shows the profile picture' do
+      visit user_path(@user)
+
+      expect(page).to have_css("img[src*='person.jpg']")
+    end
+
+    it 'shows the number of posts by the user' do
+      visit user_path(@user)
+
+      expect(page).to have_content(@user.posts.count)
+    end
+
+    it 'shows the bio of the user' do
+      visit user_path(@user)
+
+      expect(page).to have_content(@user.bio)
+    end
+
+    it 'shows the first three posts by the user' do
+      visit user_path(@user)
+
+      expect(page).to have_content(@user.posts.first.title)
+      expect(page).to have_content(@user.posts.second.title)
+      expect(page).to have_content(@user.posts.third.title)
+    end
+
+    it 'shows see all posts button' do
+      visit user_path(@user)
+
+      expect(page).to have_link('See all posts')
+    end
+  end
+
+  context 'when you click the links on the page' do
+    it 'redirects to the user posts page when the see all posts button is clicked' do
+      visit user_path(@user)
+
+      click_link('See all posts')
+
+      expect(page).to have_current_path(user_posts_path(@user))
+    end
+
+    it 'redirects to the post when the post title is clicked' do
+      visit user_path(@user)
+
+      click_link(@post.title)
+
+      expect(page).to have_current_path(user_post_path(@user, @post))
+    end
+  end
+end
