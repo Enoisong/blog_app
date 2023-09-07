890154b384cd23a81ff7ec549542b79a822a1112
Enoisong <enobongitiaba988@gmail.com>
2023-09-06 22:14:49 -0700

    add confirmable

--- config/routes.rb
+++ config/routes.rb
@@ -1,11 +1,13 @@
- Rails.application.routes.draw do  
-  devise_for :users, controllers: { confirmations: 'users/confirmations' }
-
-  root 'users#index'  
+Rails.application.routes.draw do
+  devise_for :users
+  # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
+  # Defines the root path route ("/")
+  # root "articles#index"
+  root 'users#index'
   resources :users, only: [:index, :show] do
     resources :posts, only: [:index, :show, :new, :create] do
       resources :comments, only: [:create]
       resources :likes, only: [:create]
     end
   end
-end 
+end
